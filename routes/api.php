ae3c79a020463fa8cc249b1b862cef3800f5d375
abdul falaq <falaq@logique.co.id>
2025-07-16 09:37:09 +0700

    Fix: allow cors

--- routes/api.php
+++ routes/api.php
@@ -8,11 +8,20 @@ use Illuminate\Support\Facades\Route;
 | API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
+| Here you may register API routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+// Handle CORS preflight requests
+Route::options('{any}', function () {
+    return response('', 200)
+        ->header('Access-Control-Allow-Origin', '*')
+        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+        ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin')
+        ->header('Access-Control-Allow-Credentials', 'true');
+})->where('any', '.*');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
