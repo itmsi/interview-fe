a22cf45a8242868b0293110195edd65b34f9ca4d
abdulfalaq5 <abdulfalaq5@gmail.com>
2025-12-25 08:58:18 +0700

    Fix: header

--- app/Http/Middleware/SecureHeadersMiddleware.php
+++ app/Http/Middleware/SecureHeadersMiddleware.php
@@ -29,7 +29,7 @@ class SecureHeadersMiddleware
         // Security Headers (dimodifikasi untuk CORS)
         $response->headers->set('Expect-CT', 'max-age:86400, enforce');
         $response->headers->set('X-Permitted-Cross-Domain-Policies', 'none');
-        $response->headers->set('Content-Security-Policy', "script-src 'self' https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/ https://fonts.googleapis.com/  'unsafe-eval' 'unsafe-inline' *.jsdelivr.net; object-src 'self';");
+        $response->headers->set('Content-Security-Policy', "script-src 'self' https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/ https://fonts.googleapis.com/ https://static.cloudflareinsights.com 'unsafe-eval' 'unsafe-inline' *.jsdelivr.net; object-src 'self';");
         $response->headers->set('Permissions-Policy', 'autoplay=(self), camera=(), encrypted-media=(self), fullscreen=(), geolocation=(self), gyroscope=(self), magnetometer=(), microphone=(), midi=(), payment=(), sync-xhr=(self), usb=()');
         $response->headers->set('Referrer-Policy', 'no-referrer');
         $response->headers->set('X-Content-Type-Options', 'nosniff');
